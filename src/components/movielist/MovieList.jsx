1701dc7ed02ba817a7dabba7c0bb8a5fffabf774
Caleb Adekoya <caleb123@meta.com>
2024-06-12 13:51:43 -0700

    Finalized modal

--- src/components/movielist/MovieList.jsx
+++ src/components/movielist/MovieList.jsx
@@ -110,7 +110,6 @@ export default function MovieList({searchTerm, view, onOpenModal, onCloseModal})
 
     const handleCloseModal = () => {
         setSelectedMovie(null);
-        onCloseModal();
     }
 
     useEffect(() => {
